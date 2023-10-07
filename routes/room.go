6149a1dfbb3b0fe892e775215ac4988bba5ba55f
Saharat Thongin <was.dew4546@gmail.com>
2023-10-08 02:27:17 +0700

    Organize Room route

--- routes/room.go
+++ routes/room.go
@@ -1,12 +1,18 @@
 package routes
 
 import (
+	"chicchat/controllers"
+	"chicchat/middlewares"
 	"chicchat/models"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
 
-func Room(app *fiber.App, rooms map[string]*models.RoomWebSocket) {
-	// app.Get("/room/:room", controllers.Room(rooms))
-	// app.Get("/room/:room/:id", controllers.ConnectToRoom(rooms))
+func Room(app *fiber.App, db *gorm.DB, rooms map[string]*models.RoomWebSocket) {
+	// Use the WebSocket handler
+	app.Get("/room/:id",
+		middlewares.WebSocketMiddleware(rooms),
+		middlewares.RoomAuth(),
+		controllers.WebSocketHandler(rooms))
 }
